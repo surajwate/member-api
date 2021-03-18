978ffd49dd4b42c6c1ac9d01dd54219fe9d742b0
Suraj Wate <suraj.wate@gmail.com>
2021-03-18 13:45:13 +0530

    getting one member

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from database import get_db
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
+
 
 @app.route('/member', methods=['GET'])
 def get_members():
@@ -25,11 +27,17 @@ def get_members():
 
         return_values.append(member_dict)
 
-    return jsonify({'members' : return_values})
+    return jsonify({'members': return_values})
+
 
 @app.route('/member/<int:member_id>', methods=['GET'])
 def get_member(member_id):
-    return "This return one member by id."
+    db = get_db()
+    member_cur = db.execute(
+        'select id, name, email, level from members where id = ?', [member_id])
+    member = member_cur.fetchone()
+    return jsonify({'member': {'id': member['id'], 'name': member['name'], 'email': member['email'], 'level': member['level']}})
+
 
 @app.route('/member', methods=['POST'])
 def add_member():
@@ -39,21 +47,26 @@ def add_member():
     level = new_member_data['level']
 
     db = get_db()
-    db.execute('insert into members (name, email, level) values (?, ?, ?)', [name, email, level])
+    db.execute('insert into members (name, email, level) values (?, ?, ?)', [
+               name, email, level])
     db.commit()
 
-    member_cur = db.execute('select id, name, email, level from members where name = ?', [name])
+    member_cur = db.execute(
+        'select id, name, email, level from members where name = ?', [name])
     new_member = member_cur.fetchone()
 
-    return jsonify({ 'id': new_member['id'], 'name' : new_member['name'], 'email': new_member['email'], 'level': new_member['level']})
+    return jsonify({'member': {'id': new_member['id'], 'name': new_member['name'], 'email': new_member['email'], 'level': new_member['level']}})
+
 
 @app.route('/member/<int:member_id>', methods=['PUT', 'PATCH'])
 def edit_member(member_id):
     return "This update a member by id."
 
+
 @app.route('/member/<int:member_id>', methods=['DELETE'])
 def delete_member(member_id):
     return "This removes a member by id."
+
 
 if __name__ == '__main__':
     app.run(debug=True)
